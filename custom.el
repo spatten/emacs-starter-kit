27bf13036798bf98bce8c6f45b766bc1f71e53b2
Scott Patten <spatten@scott-pattens-macbook-pro.local>
2010-07-27 12:35:09 -0700

    custom.el: adding ruby-electric, tab widths, markdown path, mouse wheel mode, line numbering, textmate

--- custom.el
+++ custom.el
@@ -1,3 +1,31 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
 (require 'textmate)
 (textmate-mode)
+
+(setq standard-indent 2)
+
+(mouse-wheel-mode t)
+
+(global-linum-mode)
+
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook (lambda () (ruby-electric-mode t)))
+
+(setq ido-use-filename-at-point nil)
+
+(auto-fill-mode 0)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(markdown-command "/usr/local/bin/markdown"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
