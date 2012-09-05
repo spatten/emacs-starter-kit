c04fc49917f443bd8630f6a57149c8446ed6f633
Scott Patten <scott@scottpatten.ca>
2012-09-05 13:57:17 -0700

    ask before closing.

--- custom.el
+++ custom.el
@@ -131,3 +131,19 @@
 ;; C-c C-r (send the highlighted region to the scheme interpreter)
 ;; from http://community.schemewiki.org/?emacs-tutorial
 (setq scheme-program-name "gsi -:d-")
+;; (setq scheme-program-name "mzscheme")
+
+(server-start)
+
+;; http://nileshk.com/2009/06/13/prompt-before-closing-emacs.html
+(defun ask-before-closing ()
+  "Ask whether or not to close, and then close if y was pressed"
+  (interactive)
+  (if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
+      (if (< emacs-major-version 22)
+          (save-buffers-kill-terminal)
+        (save-buffers-kill-emacs))
+    (message "Canceled exit")))
+
+(when window-system
+  (global-set-key (kbd "C-x C-c") 'ask-before-closing))
