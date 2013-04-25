3d35b5cd8cf0e96aac0d4b637bb805db18511427
Scott Patten <scott@scottpatten.ca>
2013-04-24 22:18:42 -0700

    updating mucho stuff

--- custom.el
+++ custom.el
@@ -1,3 +1,6 @@
+(setq initial-scratch-message "")
+(setq initial-major-mode 'text-mode)
+
 (load "~/.emacs.d/movelines.el")
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/vendor/emacs-color-theme-solarized")
@@ -38,8 +41,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(fringe-mode (quote (1 . 1)) nil (fringe))
- '(markdown-command "/usr/local/bin/markdown")
+ '(fringe-mode (quote (1 . 1)) nil (fringe))
+ '(markdown-command "/Users/spatten/.rbenv/shims/kramdown")
  '(nxml-char-ref-display-glyph-flag nil))
 
 (add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
@@ -53,53 +56,15 @@
 (require 'lorem-ipsum)
 
 ;; Lisp-flavored Erlang
-(add-to-list 'load-path "~/.emacs.d/vendor/lfe")
-(require 'lfe-mode)
-(add-to-list 'auto-mode-alist '("\.lfe$" . lfe-mode))
-(require 'lfe-start)
+;; (add-to-list 'load-path "~/.emacs.d/vendor/lfe")
+;; (require 'lfe-mode)
+;; (add-to-list 'auto-mode-alist '("\.lfe$" . lfe-mode))
+;; (require 'lfe-start)
 
 (add-to-list 'load-path "~/.emacs.d/vendor/mustache-mode.el")
 (setq auto-mode-alist (cons '("\\.tpl$" . tpl-mode) auto-mode-alist))
 (autoload 'tpl-mode "tpl-mode" "Major mode for editing CTemplate files." t)
 ;; (require 'mustache)
-
- ;; from http://www.enigmacurry.com/2008/12/26/emacs-ansi-term-tricks/
-(require 'term)
-(defun visit-ansi-term ()
-  "If the current buffer is:
-     1) a running ansi-term named *ansi-term*, rename it.
-     2) a stopped ansi-term, kill it and create a new one.
-     3) a non ansi-term, go to an already running ansi-term
-        or start a new one while killing a defunt one"
-  (interactive)
-  (let ((is-term (string= "term-mode" major-mode))
-        (is-running (term-check-proc (buffer-name)))
-        (term-cmd "/bin/bash")
-        (anon-term (get-buffer "*ansi-term*")))
-    (if is-term
-        (if is-running
-            (if (string= "*ansi-term*" (buffer-name))
-                (call-interactively 'rename-buffer)
-              (if anon-term
-                  (switch-to-buffer "*ansi-term*")
-                (ansi-term term-cmd)))
-          (kill-buffer (buffer-name))
-          (ansi-term term-cmd))
-      (if anon-term
-          (if (term-check-proc "*ansi-term*")
-              (switch-to-buffer "*ansi-term*")
-            (kill-buffer "*ansi-term*")
-            (ansi-term term-cmd))
-        (ansi-term term-cmd)))))
-(global-set-key (kbd "<f2>") 'visit-ansi-term)
-
-;; Haskell!!
-;; You can only pick one of the indentation modes
-(load "haskell-mode/haskell-site-file")
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
 ;; settings help from http://stackoverflow.com/questions/293761/how-do-i-make-emacs-show-blank-spaces
 ;; make whitespace-mode use just basic coloring
@@ -126,26 +91,26 @@
             ))   
 ;; ; turn on whitespace mode globally
 (global-whitespace-mode 1)   
-
-;; SLIME (lisp REPL)
-(add-to-list 'load-path "~/.emacs.d/slime/")  ; your SLIME directory
-(setq inferior-lisp-program "/usr/local/bin/sbcl") ; your Lisp system
-(require 'slime)
-(slime-setup '(slime-fancy))
-
-(load "my_methods")
-
-;; set scheme interpreter. Run with "M-x run-scheme"
-;; Then, you can send s-expressions to the interpreter from another window with
-;; C-x C-e (put your cursor at the end of an S-expression then do this)
-;; C-c C-r (send the highlighted region to the scheme interpreter)
-;; from http://community.schemewiki.org/?emacs-tutorial
-(setq scheme-program-name "gsi -:d-")
-;; (setq scheme-program-name "mzscheme")
-
+;; 
+;; ;; SLIME (lisp REPL)
+;; (add-to-list 'load-path "~/.emacs.d/slime/")  ; your SLIME directory
+;; (setq inferior-lisp-program "/usr/local/bin/sbcl") ; your Lisp system
+;; (require 'slime)
+;; (slime-setup '(slime-fancy))
+;; 
+;; (load "my_methods")
+;; 
+;; ;; set scheme interpreter. Run with "M-x run-scheme"
+;; ;; Then, you can send s-expressions to the interpreter from another window with
+;; ;; C-x C-e (put your cursor at the end of an S-expression then do this)
+;; ;; C-c C-r (send the highlighted region to the scheme interpreter)
+;; ;; from http://community.schemewiki.org/?emacs-tutorial
+;; (setq scheme-program-name "gsi -:d-")
+;; ;; (setq scheme-program-name "mzscheme")
+;; 
 (server-start)
-
-;; http://nileshk.com/2009/06/13/prompt-before-closing-emacs.html
+;; 
+;; ;; http://nileshk.com/2009/06/13/prompt-before-closing-emacs.html
 (defun ask-before-closing ()
   "Ask whether or not to close, and then close if y was pressed"
   (interactive)
@@ -157,28 +122,26 @@
 
 (when window-system
   (global-set-key (kbd "C-x C-c") 'ask-before-closing))
-
-;; Use PCRE regexes instead of emacs regexes
+;; 
+;; ;; Use PCRE regexes instead of emacs regexes
 (load "~/.emacs.d/vendor/pcre2el/pcre2el.el")
 (global-set-key [(meta %)] 'pcre-query-replace-regexp)
 (global-set-key [(ctrl meta %)] 'pcre-query-replace-regexp)
-
-;; full-ack extension (http://nschum.de/src/emacs/full-ack/)
-(add-to-list 'load-path "~/.emacs.d/vendor/full-ack.el")
-(autoload 'ack-same "full-ack" nil t)
-(autoload 'ack "full-ack" nil t)
-(autoload 'ack-find-same-file "full-ack" nil t)
-(autoload 'ack-find-file "full-ack" nil t)
-
-;; disable minimize window
+;; 
+;; ;; disable minimize window
 (global-unset-key "\C-x\C-z")
 ;; (setq mac-command-modifier 'super) ; sets the Command key as Super, but this is done already, so only left if I move to Unix or something crazy like that.
 (global-unset-key (kbd "s-m"))
-
-;; from http://kieranhealy.org/esk/starter-kit-bindings.html
-;; resizing 'windows' (i.e., inside the frame)
+;; 
+;; ;; from http://kieranhealy.org/esk/starter-kit-bindings.html
+;; ;; resizing 'windows' (i.e., inside the frame)
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)  
+;; 
 
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
